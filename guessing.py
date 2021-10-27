a22b4bcd54137591bf55bb5d38e250c6f8188ee8
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-10-27 18:13:46 -0300

    simple if

--- guessing.py
+++ guessing.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+print("###############################")
+print("######## Adivinhação ##########")
+print("###############################")
+
+secretValue = 32
+
+guess_str = input("Digite um número: ")
+guess = int(guess_str);
+
+print("Você digitou ", guess)
+
+if (secretValue == guess):
+    print("Você acertou")
+else:
+    print("Você errou")
+
+print("Fim do jogo")
