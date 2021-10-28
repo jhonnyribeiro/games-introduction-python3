2613fd6807399288f198484360a59c623d6d1e71
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-10-28 20:12:01 -0300

    while + if + for

--- hangman.py
+++ hangman.py
@@ -7,7 +7,13 @@ def play_hangman():
     hung = False
     got_it_right = False
     while (not hung and not got_it_right):
-        print("Jogando....")
+        guess = input("Digite uma letra: ")
+
+        index = 0
+        for letter in hidden_word:
+            if (guess == letter):
+                print("Encontrei a letra {} na posição {}".format(letter, index))
+            index = index + 1
 
     print("Fim do jogo")
 
