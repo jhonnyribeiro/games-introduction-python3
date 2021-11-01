030bca2dec8932da2fccb4ce931c29915785c9d6
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-11-01 07:50:17 -0300

    lists

--- hangman.py
+++ hangman.py
@@ -4,18 +4,23 @@ def play_hangman():
     print("###############################")
 
     hidden_word = "banana"
+    right_letters = ['_', '_', '_', '_', '_', '_']
+
     hung = False
     got_it_right = False
+
+    print(right_letters)
+
     while (not hung and not got_it_right):
         guess = input("Digite uma letra: ")
         guess = guess.strip()
 
-
         index = 0
         for letter in hidden_word:
             if (guess.upper() == letter.upper()):
-                print("Encontrei a letra {} na posição {}".format(letter, index))
+                right_letters[index] = letter
             index = index + 1
+        print(right_letters)
 
     print("Fim do jogo")
 
