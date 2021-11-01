5d280157b38ab35d41fd9f544a6fb73ed8c6fee4
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-11-01 07:30:08 -0300

    strip and upper

--- hangman.py
+++ hangman.py
@@ -8,10 +8,12 @@ def play_hangman():
     got_it_right = False
     while (not hung and not got_it_right):
         guess = input("Digite uma letra: ")
+        guess = guess.strip()
+
 
         index = 0
         for letter in hidden_word:
-            if (guess == letter):
+            if (guess.upper() == letter.upper()):
                 print("Encontrei a letra {} na posição {}".format(letter, index))
             index = index + 1
 
